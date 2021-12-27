27073dc4712df078f05d3efdca1a079df54cf7b4
josephchait <danchait10@gmail.com>
2021-12-27 14:41:12 -0500

    now check both positions

--- Gym-AI-App/RealTimeCam.py
+++ Gym-AI-App/RealTimeCam.py
@@ -58,17 +58,28 @@ while cap.isOpened():
         
         
         #here you can put the pose you want to check, and the position of that pose
-        truthArray = CheckPose('JJ', CurrentDegrees, '2')
-        image[0:30,0:30,:] = [0,200,0] if truthArray[0] else [0,0,200]
-        image[40:70,0:30,:] = [0,200,0] if truthArray[1] else [0,0,200]
-        image[80:110,0:30,:] = [0,200,0] if truthArray[2] else [0,0,200]
-        image[120:150,0:30,:] = [0,200,0] if truthArray[3] else [0,0,200]
-        image[160:190,0:30,:] = [0,200,0] if truthArray[4] else [0,0,200]
-        image[200:230,0:30,:] = [0,200,0] if truthArray[5] else [0,0,200]
-        image[240:270,0:30,:] = [0,200,0] if truthArray[6] else [0,0,200]
-        image[280:310,0:30,:] = [0,200,0] if truthArray[7] else [0,0,200]
-        image[320:350,0:30,:] = [0,200,0] if truthArray[8] else [0,0,200]
-        image[360:390,0:30,:] = [0,200,0] if truthArray[9] else [0,0,200]
+        truthArrayP1 = CheckPose('Dan', CurrentDegrees, '1')
+        truthArrayP2 = CheckPose('Dan', CurrentDegrees, '2')
+        image[0:30,0:30,:] = [0,200,0] if truthArrayP1[0] else [0,0,200]
+        image[40:70,0:30,:] = [0,200,0] if truthArrayP1[1] else [0,0,200]
+        image[80:110,0:30,:] = [0,200,0] if truthArrayP1[2] else [0,0,200]
+        image[120:150,0:30,:] = [0,200,0] if truthArrayP1[3] else [0,0,200]
+        image[160:190,0:30,:] = [0,200,0] if truthArrayP1[4] else [0,0,200]
+        image[200:230,0:30,:] = [0,200,0] if truthArrayP1[5] else [0,0,200]
+        image[240:270,0:30,:] = [0,200,0] if truthArrayP1[6] else [0,0,200]
+        image[280:310,0:30,:] = [0,200,0] if truthArrayP1[7] else [0,0,200]
+        image[320:350,0:30,:] = [0,200,0] if truthArrayP1[8] else [0,0,200]
+        image[360:390,0:30,:] = [0,200,0] if truthArrayP1[9] else [0,0,200]
+        image[0:30,40:70,:] = [0,200,0] if truthArrayP2[0] else [0,0,200]
+        image[40:70,40:70,:] = [0,200,0] if truthArrayP2[1] else [0,0,200]
+        image[80:110,40:70,:] = [0,200,0] if truthArrayP2[2] else [0,0,200]
+        image[120:150,40:70,:] = [0,200,0] if truthArrayP2[3] else [0,0,200]
+        image[160:190,40:70,:] = [0,200,0] if truthArrayP2[4] else [0,0,200]
+        image[200:230,40:70,:] = [0,200,0] if truthArrayP2[5] else [0,0,200]
+        image[240:270,40:70,:] = [0,200,0] if truthArrayP2[6] else [0,0,200]
+        image[280:310,40:70,:] = [0,200,0] if truthArrayP2[7] else [0,0,200]
+        image[320:350,40:70,:] = [0,200,0] if truthArrayP2[8] else [0,0,200]
+        image[360:390,40:70,:] = [0,200,0] if truthArrayP2[9] else [0,0,200]
         # End of checking
         
     except:
@@ -77,8 +88,8 @@ while cap.isOpened():
     cv2.imshow("Mediapipe Feed", image) #Visualize actual frame
 
     #if 's' is pressed, save the two positions recorded as a new row in the csv, under the name in addPose
-    if cv2.waitKey(10) & 0xFF == ord('q'): 
-            addPose('JJ', FirstPositionDegrees, SecondPositionDegrees)
+    if cv2.waitKey(10) & 0xFF == ord('s'): 
+            addPose('Dan', FirstPositionDegrees, SecondPositionDegrees)
             break
     
     #if 'Q' is pressed just exit
